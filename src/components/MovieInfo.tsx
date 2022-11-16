5d724e69e3bb7c5e30cb72946fc1cc1c8896845e
Gibbem <ozmehak.calle@gmail.com>
2022-11-16 05:52:31 +0100

    fixed modal styling, added top billed cast and movie description

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal';
 import {GetSingle} from "./GetSingle";
 
 
@@ -12,17 +11,13 @@ export const MovieInfo = (props: any) => {
     return (
         <>
 
-                <Modal.Header closeButton>
-                    <Modal.Title></Modal.Title>
+
+
                     <GetSingle id={props.id} />
-                </Modal.Header>
-                <Modal.Body>
 
 
-                </Modal.Body>
-                <Modal.Footer>
 
-                </Modal.Footer>
+
 
         </>
     );
