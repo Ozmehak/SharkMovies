83ae4be663d801aaea22044d04d7775f8b0d701a
Gibbem <ozmehak.calle@gmail.com>
2022-11-17 05:05:21 +0100

    user email added to cookie on consent and login, removed on logout. option to not be bothered again added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,63 @@
-import React  from "react";
-import { auth, signInWithGoogle, logout } from "../firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, {useEffect} from "react";
+import {auth, signInWithGoogle, logout} from "../firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 import Button from "react-bootstrap/Button";
+import {useCookies} from "react-cookie";
+import LogRocket from "logrocket";
+
 
 function Login() {
 
     const [user] = useAuthState(auth);
+    const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
 
-    return (
-        <div className="login">
 
-            {!user && <Button variant="outline-info" className="login__btn login__google" onClick={signInWithGoogle}>
-                Login with Google
-            </Button>}
+ useEffect(() => {
 
-            {user && <div>{user.displayName}<Button variant="outline-dark" onClick={logout}>LogOut</Button></div>}
+     if (user) {setCookie("cookie-from-hell", `${user.email}`)}
 
-        </div>
-    );
+ }, [user])
+
+
+
+
+
+
+
+
+    const cookieMaster = () => {
+        signInWithGoogle()
+        LogRocket.init("znathy/mango-tree");
+        let expireDate = new Date();
+        expireDate.setTime(expireDate.getTime() + 1 * 3600 * 1000);
+        setCookie("cookie-from-hell", `Adam`, {
+            expires: expireDate,
+            httpOnly: false,
+            path: "/",
+            secure: true,
+        });
+
+    }
+
+    const cookieCruncher = () => {
+        removeCookie("cookie-from-hell")
+        logout()
+    }
+
+
+
+
+
+return (
+    <div className="login">
+
+        {!user && <Button variant="outline-info" className="login__btn login__google" onClick={cookieMaster}>
+            Login with Google
+        </Button>}
+
+        {user && <div>{user.displayName}<Button variant="outline-dark" onClick={cookieCruncher}>LogOut</Button></div>}
+
+    </div>
+)
 }
 export default Login;
